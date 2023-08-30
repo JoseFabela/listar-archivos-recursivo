caf6754451ac8b98ad387b2f6ddd657265d4b2b5
JoseFabela <jfabela522@gmail.com>
2023-08-30 17:34:25 -0600

    Agregar archivos de proyecto.

---
+++ listar archivos recursivo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listar_archivos_recursivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\"; // Cambia esto a la ruta del directorio que desees explorar
+            ListFilesRecursively(directoryPath);
+            Console.ReadLine();
+        }
+        static void ListFilesRecursively(string directoryPath)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(directoryPath))
+                {
+                    Console.WriteLine($"Archivo: {Path.GetFileName(file)}, Extensión: {Path.GetExtension(file)}");
+                }
+
+                foreach (string subdirectory in Directory.GetDirectories(directoryPath))
+                {
+                    ListFilesRecursively(subdirectory);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error al acceder a {directoryPath}: {e.Message}");
+            }
+        }
+    }
+}
